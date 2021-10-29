f32bf24da2e6cbab6e94c42416dfd4fdddd9c5c5
SamG1002 <samuzinhoguerra@gmail.com>
2021-10-29 16:59:30 -0300

    meu ip adicionado

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -4,7 +4,8 @@ const api = axios.create({
   // Seu ip abaixo 👇
   // David: 192.168.0.17
   // Mari: 192.168.15.26'
-  baseURL: 'http://192.168.0.17:3333',
+  // Samuel: 192.168.15.114
+  baseURL: 'http://192.168.15.114:3333',
 });
 
 export default api;
