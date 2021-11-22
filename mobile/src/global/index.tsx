4631aa01d394d4610d269274cc9729af7038957b
Almeida154 <davidalmeida154of@gmail.com>
2021-11-22 19:20:15 -0300

    :globe_with_meridians: Populando o DEFAULT_ICONS_CATEGORYACCOUNT

--- mobile/src/global/index.tsx
+++ mobile/src/global/index.tsx
@@ -203,31 +203,608 @@ export default {
     },
   ],
   DEFAULT_ICONS_CATEGORYACCOUNT: [
-    { description: 'Caixa', icon: 'https://logodownload.org/wp-content/uploads/2014/02/caixa-logo.jpg' },
-    { description: 'Banco do Brasil', icon: 'https://play-lh.googleusercontent.com/1-aNhsSPNqiVluwNGZar_7F5PbQ4u1zteuJ1jumnArhe8bfYHHaVwu4aVOF5-NAmLaA' },
     {
-      description: 'Itaú', icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banco_Ita%C3%BA_logo.svg/1200px-Banco_Ita%C3%BA_logo.svg.png',
+      description: 'ABC Brasil',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Acesso Banco',
+      accent: '#00bf66',
+    },
+
+    {
+      description: 'ActivTrades',
+      accent: '#040c1b',
+    },
+
+    {
+      description: 'Agibank',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Ágora Investimentos',
+      accent: '#002529',
+    },
+
+    {
+      description: 'alelo',
+      accent: '#7f9525',
+    },
+
+    {
+      description: 'Ame Digital',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Avenue Securitie',
+      accent: '#1d1d1d',
+    },
+
+    {
+      description: 'B2S',
+      accent: '#231d98',
+    },
+
+    {
+      description: 'Banco Cacique',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Banco da Amazônia',
+      accent: '#052a15',
+    },
+
+    {
+      description: 'Banco do Brasil',
+      accent: '#afaf33',
+    },
+
+    {
+      description: 'Banco do Nordeste',
+      accent: '#5d0f22',
+    },
+
+    {
+      description: 'Banco Inter',
+      accent: '#d7d7d7',
+    },
+
+    {
+      description: 'Banco Original',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Banco PAN',
+      accent: '#0c8ec9',
+    },
+
+    {
+      description: 'Banco Sofisa direto',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Banco Votorantim',
+      accent: '#034c2e',
+    },
+
+    {
+      description: 'Banestes',
+      accent: '#003365',
+    },
+
+    {
+      description: 'Banif',
+      accent: '#660075',
+    },
+
+    {
+      description: 'Banpara',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Banrisul',
+      accent: '#005889',
+    },
+
+    {
+      description: 'bbm',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'BBVA',
+      accent: '#003365',
+    },
+
+    {
+      description: 'Ben Visa Vale',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Binance',
+      accent: '#232323',
+    },
+
+    {
+      description: 'BGM',
+      accent: '#cd7318',
+    },
+
+    {
+      description: 'Bradesco',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'BRB',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'BRDE',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'BTG Pactual',
+      accent: '#000c32',
+    },
+
+    {
+      description: 'BTG+',
+      accent: '#232323',
+    },
+
+    {
+      description: 'C6 Bank',
+      accent: '#141414',
+    },
+
+    {
+      description: 'Caixa',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Carrefour',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Celcoin',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Cetelem',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Citibank',
+      accent: '#003365',
+    },
+
+    {
+      description: 'Clear Investimentos',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Cruzeirodosul',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Daycoval',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Desjardins Bank',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Digi+',
+      accent: '#000624',
+    },
+
+    {
+      description: 'Digio',
+      accent: '#000b3d',
+    },
+
+    {
+      description: 'Diin',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Easynvest',
+      accent: '#d9d9b8',
+    },
+
+    {
+      description: 'EbanxGo',
+      accent: '#171b21',
+    },
+
+    {
+      description: 'Elliot',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Genial Investimentos',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Havan',
+      accent: '#002959',
+    },
+
+    {
+      description: 'Hotmart',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'HSBC',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Icatu',
+      accent: '#002959',
+    },
+
+    {
+      description: 'IQ Option',
+      accent: '#003365',
+    },
+
+    {
+      description: 'Itaú Personnalite',
+      accent: '#050914',
+    },
+
+    {
+      description: 'Itaú',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Iti',
+      accent: '#bb0671',
+    },
+
+    {
+      description: 'Juno',
+      accent: '#0d14a8',
+    },
+
+    {
+      description: 'LHV',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Magnetis',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Méliuz',
+      accent: '#cb1e3a',
+    },
+
+    {
+      description: 'Mercado Bitcoin',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'MercadoPago',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Mercantil do Brasil',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'ModalMais',
+      accent: '#191a21',
+    },
+
+    {
+      description: 'Moip',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Monetizze',
+      accent: '#00345a',
+    },
+
+    {
+      description: 'Monetus',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'N26',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Neon',
+      accent: '#07aac3',
+    },
+
+    {
+      description: 'Next',
+      accent: '#01bb46',
+    },
+
+    {
+      description: 'Nova Futura Investimentos',
+      accent: '#ececec',
     },
     {
-      description: 'Moeda em círculo', icon: 'MaterialCommunityIcons:currency-usd-circle'
+      description: 'NovaDAX',
+      accent: '#cac7bf',
     },
-    { description: 'Banco da Amazônia', icon: 'https://projeto-cdn.infra.grancursosonline.com.br/basa.png' },
+
+    {
+      description: 'NuBank',
+      accent: '#6306a0',
+    },
+
+    {
+      description: 'Órama',
+      accent: '#01571d',
+    },
+
+    {
+      description: 'Pag!',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'PagBank',
+      accent: '#01571d',
+    },
+
+    {
+      description: 'PagSeguro',
+      accent: '#009a7e',
+    },
+
+    {
+      description: 'Paypal',
+      accent: '#00488d',
+    },
+
+    {
+      description: 'PayU Brasil',
+      accent: '#a0ae00',
+    },
+
+    {
+      description: 'PicPay',
+      accent: '#489e39',
+    },
+
+    {
+      description: 'Porto Seguro',
+      accent: '#ececec',
+    },
+    {
+      description: 'Primacredi',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'RCI',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Recargapay',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Rico',
+      accent: '#cf4502',
+    },
+
+    {
+      description: 'Safra',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Santander',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Shoptime',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Sicoob',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Sicredi',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Social Bank',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Sodexo',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Sumup',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Tangerine',
+      accent: '#ca610d',
+    },
+
+    {
+      description: 'TD AmeriTrade',
+      accent: '#ececec',
+    },
+    {
+      description: 'Tesouro Direto',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Tesouro Nacional',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Ticket',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Toro Investimentos',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Tribanco',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Tudo Azul',
+      accent: '#000d24',
+    },
+
+    {
+      description: 'Unicred',
+      accent: '#012a1a',
+    },
+
+    {
+      description: 'Uniprime',
+      accent: '#003850',
+    },
+
+    {
+      description: 'Urbe.Me',
+      accent: '#000d24',
+    },
+
+    {
+      description: 'UrPay',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Viacredi',
+      accent: '#004661',
+    },
+
+    {
+      description: 'Vitreo',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Warren',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Wirecard',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Woop',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Xdex',
+      accent: '#ececec',
+    },
+
+    {
+      description: 'Xp Investimentos',
+      accent: '#02131d',
+    },
+
+    {
+      description: 'Caixa',
+      icon: 'https://logodownload.org/wp-content/uploads/2014/02/caixa-logo.jpg',
+    },
+    {
+      description: 'Banco do Brasil',
+      icon: 'https://play-lh.googleusercontent.com/1-aNhsSPNqiVluwNGZar_7F5PbQ4u1zteuJ1jumnArhe8bfYHHaVwu4aVOF5-NAmLaA',
+    },
+    {
+      description: 'Itaú',
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banco_Ita%C3%BA_logo.svg/1200px-Banco_Ita%C3%BA_logo.svg.png',
+    },
+    {
+      description: 'Moeda em círculo',
+      icon: 'MaterialCommunityIcons:currency-usd-circle',
+    },
+    {
+      description: 'Banco da Amazônia',
+      icon: 'https://projeto-cdn.infra.grancursosonline.com.br/basa.png',
+    },
     { description: 'Nubank', icon: 'https://t2.tudocdn.net/354716?w=1200' },
     {
-      description: 'Bradesco', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg1QN1uIngc6pkX2Zg5Mn_mvZ501sC6x0luIz9TQ1W1jUi8zTSCiwSOMmJKs-uNiu6uXw&usqp=CAU'
+      description: 'Bradesco',
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg1QN1uIngc6pkX2Zg5Mn_mvZ501sC6x0luIz9TQ1W1jUi8zTSCiwSOMmJKs-uNiu6uXw&usqp=CAU',
     },
     {
-      description: 'Carteira', icon: 'Entypo:wallet'
+      description: 'Carteira',
+      icon: 'Entypo:wallet',
     },
     {
-      description: 'Banco Safra', icon: 'https://seeklogo.com/images/B/Banco_Safra-logo-517D130E32-seeklogo.com.png',
+      description: 'Banco Safra',
+      icon: 'https://seeklogo.com/images/B/Banco_Safra-logo-517D130E32-seeklogo.com.png',
     },
     {
-      description: 'Gráfico', icon: 'MaterialIcons:show-chart'
+      description: 'Gráfico',
+      icon: 'MaterialIcons:show-chart',
     },
     {
-      description: 'Banco Inter', icon: 'https://play-lh.googleusercontent.com/FDCnEZ5wKIYucB6n63jQC8tymfmoaNtM2GScjDP5fuKzG4bjXqWshysyfDvxccVYwg',
+      description: 'Banco Inter',
+      icon: 'https://play-lh.googleusercontent.com/FDCnEZ5wKIYucB6n63jQC8tymfmoaNtM2GScjDP5fuKzG4bjXqWshysyfDvxccVYwg',
     },
-    
-  ]
+  ],
 };
